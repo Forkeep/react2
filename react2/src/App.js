518d3c09caff2128e96673f14ad55896b8572c59
Forkeep <13126593533@163.com>
2019-03-20 08:21:18 +0800

    map(index,item)

--- react2/src/App.js
+++ react2/src/App.js
@@ -1,25 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+      inputValue: ""
+    };
+  }
+  handleBtnClick() {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ""
+    });
+  }
+
+  handleInputChange(e){
+    this.setState({
+      inputValue:e.target.value,
+    })
+  }
+
+  handleItemClick(index){
+    const list=[...this.state.list];
+    list.splice(index,1);
+    this.setState({
+      list:list
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div>
+          <input
+            value={this.state.inputValue}
+            onChange={this.handleInputChange.bind(this)}
+          />
+          <button onClick={this.handleBtnClick.bind(this)}>add</button>
+        </div>
+        <div>
+          <ul>
+            {/* 注意此处map(item,index)的顺序不可以随意调换 */}
+            {this.state.list.map((item, index) => {
+              return (
+                <li
+                  key={index}
+                  onClick={this.handleItemClick.bind(this, index)}
+                >
+                  {item}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     );
   }

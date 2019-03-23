0d2cd691f680580253987287d64f2c6a93e4f6bd
Forkeep <13126593533@163.com>
2019-03-23 08:15:45 +0800

    Ï

--- reacte3/src/App.js
+++ reacte3/src/App.js
@@ -1,25 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // 此处this.state 不是函数
+    this.state = {
+      list: [],
+      inputValue: ""
+    };
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      inputValue: e.target.value
+    });
+  }
+
+  handleBtnClick() {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ""
+    });
+  }
+  hadleItemClick(index) {
+    const list1 = this.state.list;
+    list1.splice(index, 1);
+    this.setState({
+      list: list1
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <input
+          value={this.state.inputValue}
+          onChange={this.handleInputChange.bind(this)}
+        />
+        <button onClick={this.handleBtnClick.bind(this)}>add</button>
+        <div>
+          <ul>
+            {this.state.list.map((item, index) => {
+              return (
+                <li key={index} onClick={this.hadleItemClick.bind(this, index)}>
+                  {item}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     );
   }
